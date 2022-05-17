208b209e59eb517054ee88bfe03fbd3bd2b45929
Daniel Españadero <developer@daniel-espanadero.com>
2022-05-17 04:00:57 +0200

    Express-validator added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { check } from 'express-validator';
+
 import { signUp, logIn } from '../controllers/auth';
+import { checkFields } from '../middlewares/check-fields';
 
 const router = Router();
 
-router.post('/signup', signUp);
+router.post('/signup', [
+    check('username', 'Username is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'The password must have: Minimum 8 characters, maximum 15, at least one capital letter, at least one lowercase letter, at least one digit, no blanks, at least 1 special character').not().isEmpty().isLength({ min: 8, max: 15 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&](?=.*[!@#$%^&*])/),
+    checkFields
+], signUp);
 router.post('/login', logIn);
 
 export default router;
